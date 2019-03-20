374b0953ac3994bdb76cab1a6967b98d76ec7b7b
ivansd75 <ivansantaeufemia@gmail.com>
2019-03-20 10:24:33 +0100

    Base de datos de nombres creada

--- index.js
+++ index.js
@@ -16,6 +16,21 @@ app.get("/",function(req,res){
   res.sendFile(__dirname+"/index.html");
 });
 
+app.post("/",function(req,res){
+  //Recojo los valores que envian desde el Formulario
+  console.log(req.body);
+  var sexo=(req.body.sexo);
+  var apellidoPaterno=(req.body.apadre);
+  var apellidoMaterno=(req.body.amadre);
+
+  //Declaro 2 arrays de NOMBRES
+  var nombresNene=["Andrés","Julián","Pelayo","Martin","Rodrigo"];
+  var nombresNena=["Jimena","Sandra","Andrea","Petra","Lucía"];
+  //Escogemos aleatoriamente un nombre
+  
+
+});
+
 //Arranca el servidor
 app.listen(3000,function(){
   console.log("Servidor escuchando en puerto 3000.");
